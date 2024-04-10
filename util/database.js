edc6100260815fe72ecc0d647dae3e24f849a656
Divisha <divishajain07@gmail.com>
2024-04-10 15:53:23 +0530

    changes

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(`${process.env.DB_DATABASE}`, "root", `${process.env.DB_PASSWORD}`, {
+const sequelize = new Sequelize(`${process.env.MYSQL_DATABASE}`, `${process.env.MYSQL_USERNAME}`, `${process.env.MYSQL_PASSWORD}`, {
     dialect: "mysql",
     host: "localhost"
 });
